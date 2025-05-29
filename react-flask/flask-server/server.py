b62e864793eab04c87692f1f37c3431747eb5f2d
kb42 <kb42@illinois.edu>
2025-05-29 18:17:29 -0500

    bug fixes 1

--- react-flask/flask-server/server.py
+++ react-flask/flask-server/server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, '../../doc/database.db')
+DB_PATH = os.path.join(BASE_DIR, 'database.db')
 
 def get_db():
     """
